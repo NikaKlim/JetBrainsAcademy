77f7dde3bf3cd6aa0c28d6f7fd09d519dc07c033
Tetiana Klimenko <91628959+NikaKlim@users.noreply.github.com>
2023-06-08 14:19:21 +0200

    Create Stage 3.py

---
+++ Currency Converter/Stage 3.py
@@ -0,0 +1,11 @@
+print("Please, enter the number of conicoins you have:")
+
+n = input()
+
+print("Please, enter the exchange rate:")
+
+rate = input()
+
+val = float(n) * float(rate)
+
+print("The total amount of dollars:",val)
