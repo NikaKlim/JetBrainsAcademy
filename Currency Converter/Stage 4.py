b4482e8067798ee4d43c8c244697081148669e74
Tetiana Klimenko <91628959+NikaKlim@users.noreply.github.com>
2023-06-08 14:19:53 +0200

    Create Stage 4.py

---
+++ Currency Converter/Stage 4.py
@@ -0,0 +1,11 @@
+n = input()
+
+print("I will get {} RUB from the sale of {} conicoins.".format((float(n) * 2.98),float(n)))
+
+print("I will get {} ARS from the sale of {} conicoins.".format((float(n) * 0.82),float(n)))
+
+print("I will get {} HNL from the sale of {} conicoins.".format((float(n) * 0.17),float(n)))
+
+print("I will get {} AUD from the sale of {} conicoins.".format((float(n) * 1.9622),float(n)))
+
+print("I will get {} MAD from the sale of {} conicoins.".format((float(n) * 0.208),float(n)))
